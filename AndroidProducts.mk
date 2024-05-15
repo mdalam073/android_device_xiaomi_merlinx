ac6f623765836af61c8c0248a7787596c85640c4
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-05-15 14:57:55 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_merlinx.mk
+    $(LOCAL_DIR)/voltage_merlinx.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_merlinx-user \
-    lineage_merlinx-userdebug \
-    lineage_merlinx-eng
+    voltage_merlinx-ap1a-user \
+    voltage_merlinx-ap1a-userdebug \
+    voltage_merlinx-ap1a-eng
